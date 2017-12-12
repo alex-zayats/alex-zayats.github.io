cc728911d1bccb15d0877fe9b9cf12af108b9567
Alex <alex3953898@yandex.com>
2017-12-13 02:19:29 +0300

    removed commented code

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
 'use strict';
 
-// import '../style/style.css';
-// import loadChannels from './channels.js';
-// import loadNews from './news.js';
-// loadChannels();
-
 require.ensure([], function() {
   require('../style/style.css');
   var loadChannels = require('./channels.js');
